f1ac795dae2a5b8de2ca2addb283ff12d6d37b65
SATHYANARAYANANVSN <16sathyanarayanan@gmail.com>
2025-07-03 12:04:55 +0530

    login page edit with animation

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -2,9 +2,11 @@
 import Image from "next/image";
 import React, { useState } from "react";
 import lampimg from "../assets/images/lamp.png";
-import potimg from "../assets/images/pot.png";
+import goml from "../assets/goml.png";
 import "./login.css";
 import { User, EyeOff } from "lucide-react";
+import Lottie from "lottie-react";
+import loginpAnimation from "../assets/animations/Animation-login.json";
 
 const Login = () => {
   // State variables
@@ -21,31 +23,20 @@ const Login = () => {
     <div className="main">
       <div className="overlay">
         <div className="content">
-          <div className="lampcontent">
-            <div className="lamp lamp1">
-              <Image src={lampimg} alt="Lamp 1" width={200} height={200} />
-            </div>
-            <div className="lamp lamp2">
-              <Image src={lampimg} alt="Lamp 2" width={200} height={200} />
-            </div>
-            <div className="lamp lamp3">
-              <Image src={lampimg} alt="Lamp 3" width={200} height={200} />
-            </div>
-            <div className="lamp lamp4">
-              <Image src={lampimg} alt="Lamp 4" width={200} height={200} />
-            </div>
+          <div className="animation-container w-[50%] h-full">
+            <Lottie
+              animationData={loginpAnimation}
+              autoplay
+              loop
+              className="w-full h-full"
+            />
           </div>
-
-          <div className="login-container">
-            <div className="pot pot1">
-              <Image src={potimg} alt="pot 1" width={120} height={120} />
-            </div>
-
-            <div className="login-card">
-              <h2 className="login-title">WELCOME BACK!</h2>
-
+          <div className="login-container ">
+            <div className="login-card  ">
+              <Image src={goml} alt="Logo" width={150} height={150} />
+              <h2 className="login-title">Login</h2>
               <div className="input-group">
-                <User className="icon" />
+                <User className="icon" size={20} color="#c54a00" />
                 <input
                   type="text"
                   placeholder="Username"
@@ -56,7 +47,7 @@ const Login = () => {
               </div>
 
               <div className="input-group">
-                <EyeOff className="icon" />
+                <EyeOff className="icon" size={20} color="#c54a00" />
                 <input
                   type="password"
                   placeholder="Password"
@@ -69,10 +60,6 @@ const Login = () => {
               <button className="login-button" onClick={handleLogin}>
                 LOGIN
               </button>
-            </div>
-
-            <div className="pot pot2">
-              <Image src={potimg} alt="pot 2" width={120} height={120} />
             </div>
           </div>
         </div>
